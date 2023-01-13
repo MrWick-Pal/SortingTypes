9281cd82b967487aae9a0107a41b05c60050705b
Matrix <cfifgfkm@gmail.com>
2023-01-13 21:26:45 +0300

    Добавлен пример сортировки выбором

--- SortingTypes/Program.cs
+++ SortingTypes/Program.cs
@@ -1,12 +1,20 @@
-﻿// 1 2 3 4 5 1 2 3
-// min -> max
-// 0 1 2 3 4 5 6 7
-// 7 2 3 4 5 1 2 3
-// 1 6 3 4 5 7 2 3
-// 1 2 3 4 5 7 6 3
-// 1 2 3 4 5 7 6 3
-// 1 2 3 3 5 7 6 4
-// 1 2 3 3 4 7 6 5
-// 1 2 3 3 4 5 6 7
-// 1 2 3 3 4 5 6 7
-// 1 2 3 3 4 5 6 7
+﻿void SortSelection (int [] collection)
+{
+    int size = collection.Length;
+    for (int i = 0; i < size - 1; i++)
+    {
+        int pos = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (collection [j] < collection [i]) pos = j;
+        }
+        int temp = collection [i];
+        collection [i] = collection [pos];
+        collection [pos] = temp;
+    }
+}
+
+int [] array = {7, 2, 3, 4, 5, 1, 2, 3 };
+
+Console.WriteLine($"[{String.Join(',', array)}]");
+SortSelection(array);
